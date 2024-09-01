0d88811c99864999d9252e0317c2a6303447406c
nirmala-vibgyor <nirmalawrk@gmail.com>
2024-09-01 06:13:41 +0530

    added views and urls

---
+++ polls/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    # 127.0.0.1/polls/
+    path('<int:question_id>', views.detail, name="detail"),
+    # 127.0.0.1/polls/1
+    path('<int:question_id>/results', views.results, name="results"),
+    # 127.0.0.1/polls/1/results
+    path('<int:question_id>/vote', views.vote, name="vote"),
+    # 127.0.0.1/polls/1/votes
+]
