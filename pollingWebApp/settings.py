1aba5c2e7480bea0e67d22e9f88eb0c948978de2
nirmala-vibgyor <nirmalawrk@gmail.com>
2024-09-01 18:18:10 +0530

    added models, views, static folder; polling features added

--- pollingWebApp/settings.py
+++ pollingWebApp/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # my app
-    'polls'
+    'polls',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
     path('', views.index, name="index"),
     # 127.0.0.1/polls/
-    path('<int:question_id>', views.detail, name="detail"),
+    path('<int:question_id>/', views.detail, name="detail"),
     # 127.0.0.1/polls/1
     path('<int:question_id>/results', views.results, name="results"),
     # 127.0.0.1/polls/1/results
